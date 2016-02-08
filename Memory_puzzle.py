758c17a986912e73c1bf44a4014a9f873c747113
divyabiyani <divyabiyani26@gmail.com>
2016-02-08 23:49:04 +0530

    Few Additions

--- Memory_puzzle.py
+++ Memory_puzzle.py
@@ -44,12 +44,16 @@ def main():
 	FramesPerSecond=pygame.time.Clock()
 	DisplaySurf=pygame.display.set_mode((WindowWidth,WindowHeight))
 	pygame.display.caption('Memory Game')
+	DisplaySurf.fill(BgColor)
 
 	mouseX=0
 	mouseY=0
 
 	mainBoard=getRandomizedBoard()
 	revealedBoxes=generateRevealedBoxesData(False)
+
+	firstSelection=None
+
 
 def getRandomizedBoard():
 	icons=[]
@@ -76,4 +80,52 @@ def generateRevealedBoxesData(val):
 	for i in range(BoardWidth):
 		revealedBoxes.append([val]*BoardHeight)
 	return revealedBoxes
+
+def startGameAnimation(mainBoard):
+	coveredBoxes=generateRevealedBoxesData(False)
+	boxes=[]
+	for x in range(BoardWidth):
+		for y in range(BoardHeight):
+			boxes.append((x,y))
+	random.shuffle(boxes)
+	boxIntoGroups=splitIntoGroupsOf(8,boxes)
+
+def splitIntoGroupsOf(size,List):
+	result=[]
+	for i in range(0,len(List),size):
+		result.append(List[i:i+size])
+	return result
+
+def drawBoard(mainBoard,revealed):
+	for boxX in range(BoardWidth):
+		for boxY in range(BoardHeight):
+			left,top=leftTopCoordsOfBox(boxX,boxY)
+			if not revealed[boxX][boxY]:
+				pygame.draw.rect(DisplaySurf,BoxColor,(left,top,BoxSize,BoxSize))
+			else:
+				shape,color=getShapeAndColor(mainBoard,boxX,boxY)
+
+def leftTopCoordsOfBox(boxX,boxY):
+	left=boxX*(BoxSize+GapSize)+XMargin
+	top=boxY*(BoxSize+GapSize)+YMargin
+	return left,top
+
+def getShapeAndColor(mainBoard,boxX,boxY):
+	return mainBoard[boxX][boxY][0],mainBoard[boxX][boxY][1]
+
+def drawIcon(shape,color,boxX,boxY):
+	quarter=int(BoxSize*0.25)
+	half=int(BoxSize*0.5)
+
+	left,top=leftTopCoordsOfBox(boxX,boxY)
+
+	if shape==Donut:
+		pygame.draw.circle(DisplaySurf,color,(left+half,top+half),half-5)
+		pygame.draw.circle(DisplaySurf,BgColor,(left+quarter,top+quarter),quarter-5)
+	elif shape==Square:
+		pygame.draw.rect(DisplaySurf,color,(left+quarter,top+quarter,BoxSize-half,BoxSize-half))
+	elif shape==Diamond:
+		pygame.draw.polygon(DisplaySurf,color,(left+half,top),)
+		
+
 
